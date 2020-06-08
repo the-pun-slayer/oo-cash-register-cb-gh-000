9928d30969dcaa88f262f5430a0d278b09399601
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 14:33:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,17 +9,17 @@ class CashRegister
   end
 
   def add_item(title, amount, quantity=1)
-    total += amount * quantity
+    @total += amount * quantity
     quantity.times do
       items << title
     end
-    self.last_transaction = amount * quantity
+    @last_transaction = amount * quantity
   end
 
   def apply_discount
     if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
+      @total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
     end
