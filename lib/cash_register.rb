015f5689cb1b62faf5e427913a980c20f981053a
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 14:17:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,15 @@
 class CashRegister
   
+  attr_accessor :total
   
   def initialize(discount=0)
     @discount=discount
     @total=0
   end
   
-  def total
-    @total
-  end
+  
+  
+ 
   
   
   
